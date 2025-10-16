ab6c9ff0a45461142ccc46df7c9c1f387f13b1ba
Tan Dat <trantandat0723@gmail.com>
2025-10-16 14:39:33 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { IoMdSearch, } from 'react-icons/io'
+import { BsCloudHaze2Fill, } from 'react-icons/bs'
+import { TbTemperatureCelsius } from 'react-icons/tb'
+import { ImSpinner8 } from 'react-icons/im'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
